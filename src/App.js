75a55ba4b67e830812148ade95cd550f97e52536
James <jdserruya@gmail.com>
2018-08-21 17:56:55 +0100

    Added particles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className='particles' params={particlesOptions}/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
